fb2907d04562d20c7fe00e03e4cbb059e729431a
Davi Teodoro Novais <davitrynity@gmail.com>
2026-05-08 22:32:06 -0300

    Refactor button handling and add LDR control

--- CS-CP02-Arduino/CS-CP02-Arduino.ino
+++ CS-CP02-Arduino/CS-CP02-Arduino.ino
@@ -3,94 +3,103 @@
 Servo servo1;
 Servo servo2;
 
+// Pinos
 int pinoServo1 = 9;
 int pinoServo2 = 10;
 int pinoLED = 11;
+int pinoLDR = A0;
 
-int btn1Up = 2;
-int btn1Down = 3;
-int btn2Right = 4;
-int btn2Left = 5;
-int btnLED = 6;
+// Botões
+int btn1 = 2;
+int btn2 = 3;
+int btn3 = 4;
+int btn4 = 5;
+int btn5 = 6;
 
+// Estados
 int angulo1 = 90;
 int angulo2 = 90;
+int passo = 15;
 
-int passo = 30;
-
-// Estado anterior dos botões
-bool estadoAnt_btn1Up = HIGH;
-bool estadoAnt_btn1Down = HIGH;
-bool estadoAnt_btn2Right = HIGH;
-bool estadoAnt_btn2Left = HIGH;
-bool estadoAnt_btnLED = HIGH;
+// Controle LDR
+bool modoLDR = false;
+unsigned long tempoLDR = 0;
+int limiarLDR = 600; // Regular conforme a luminosidade da sala e dos LEDs
 
 void setup() {
-  Serial.begin(9600);
   servo1.attach(pinoServo1);
   servo2.attach(pinoServo2);
 
-  pinMode(btn1Up, INPUT_PULLUP);
-  pinMode(btn1Down, INPUT_PULLUP);
-  pinMode(btn2Right, INPUT_PULLUP);
-  pinMode(btn2Left, INPUT_PULLUP);
-  pinMode(btnLED, INPUT_PULLUP);
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+  pinMode(btn3, INPUT_PULLUP);
+  pinMode(btn4, INPUT_PULLUP);
+  pinMode(btn5, INPUT_PULLUP);
+
   pinMode(pinoLED, OUTPUT);
 
   servo1.write(angulo1);
   servo2.write(angulo2);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Leitura atual
-  bool leitura_btn1Up = digitalRead(btn1Up);
-  bool leitura_btn1Down = digitalRead(btn1Down);
-  bool leitura_btn2Right = digitalRead(btn2Right);
-  bool leitura_btn2Left = digitalRead(btn2Left);
-  bool leitura_btnLED = digitalRead(btnLED);
 
-  // Servo 1 - aumentar
-  if (estadoAnt_btn1Up == HIGH && leitura_btn1Up == LOW) {
+  int leituraLDR = analogRead(pinoLDR);
+  Serial.println(analogRead(A0));
+
+  // ===== PRIORIDADE DO LDR =====
+  if (leituraLDR > limiarLDR && !modoLDR) {
+    modoLDR = true;
+    tempoLDR = millis();
+
+    servo1.write(90);
+    servo2.write(90);
+  }
+
+  if (modoLDR) {
+    if (millis() - tempoLDR < 10000) {
+      return; // trava sistema por 10s
+    } else {
+      modoLDR = false;
+    }
+  }
+
+  // ===== BOTÕES =====
+
+  if (digitalRead(btn1) == LOW) {
     angulo1 += passo;
     if (angulo1 > 180) angulo1 = 180;
     servo1.write(angulo1);
+    delay(200);
   }
 
-  // Servo 1 - diminuir
-  if (estadoAnt_btn1Down == HIGH && leitura_btn1Down == LOW) {
+  if (digitalRead(btn2) == LOW) {
     angulo1 -= passo;
     if (angulo1 < 0) angulo1 = 0;
     servo1.write(angulo1);
+    delay(200);
   }
 
-  // Servo 2 - aumentar
-  if (estadoAnt_btn2Right == HIGH && leitura_btn2Right == LOW) {
+  if (digitalRead(btn3) == LOW) {
     angulo2 += passo;
     if (angulo2 > 180) angulo2 = 180;
     servo2.write(angulo2);
+    delay(200);
   }
 
-  // Servo 2 - diminuir
-  if (estadoAnt_btn2Left == HIGH && leitura_btn2Left == LOW) {
+  if (digitalRead(btn4) == LOW) {
     angulo2 -= passo;
     if (angulo2 < 0) angulo2 = 0;
     servo2.write(angulo2);
+    delay(200);
   }
 
-  if (estadoAnt_btnLED == HIGH && leitura_btnLED == LOW) {
-  digitalWrite(pinoLED, HIGH);
-  delay(1000);
-  digitalWrite(pinoLED, LOW);
+  if (digitalRead(btn5) == LOW) {
+    digitalWrite(pinoLED, HIGH);
+    delay(1000);
+    digitalWrite(pinoLED, LOW);
   }
-
-  // Atualiza estados anteriores
-  estadoAnt_btn1Up = leitura_btn1Up;
-  estadoAnt_btn1Down = leitura_btn1Down;
-  estadoAnt_btn2Right = leitura_btn2Right;
-  estadoAnt_btn2Left = leitura_btn2Left;
-  estadoAnt_btnLED = leitura_btnLED;
-
-  delay(10); // pequeno debounce
-
-
+  
 }
